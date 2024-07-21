c1dcae00d98fff3597d475b51239e32a084ef157
uJoeLu <joel.neves37@gmail.com>
2024-07-20 21:29:29 -0300

    sei nao

--- apresentacao/Home.java
+++ apresentacao/Home.java
@@ -4,31 +4,29 @@ import java.util.Scanner;
 
 public class Home {
     public static void main(String[] args) {
-        try {
 
-            Scanner sc = new Scanner(System.in);
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
 
-            while (true) {
+        while (true) {
 
-                System.out.println("Qual das operações desejar executar: \n1 - Cadastrar cliente \n2 - Sair");
-                int opcao = sc.nextInt();
-                switch (opcao) {
-                    case 1:
-                        TelaCliente telaCliente = new TelaCliente();
-                        telaCliente.operacoesCliente();
-                        break;
-                    case 2:
-                        System.exit(0);
+            System.out.println("Qual das operações desejar executar: \n1 - Cadastrar cliente \n2 - Sair");
+            int opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    TelaCliente telaCliente = new TelaCliente();
+                    telaCliente.operacoesCliente();
+                    break;
+                case 2:
+                    System.exit(0);
 
-                    default:
-                        System.err.println("Operação invalida");
-                        break;
-                }
-
+                default:
+                    System.err.println("Operação invalida");
+                    break;
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+
         }
+
     }
 
 }
