780364aaa0d2c0977d4399a309d205f54e3c8c7c
uJoeLu <144572880+uJoeLu@users.noreply.github.com>
2024-07-22 14:44:20 -0300

    cliente

--- entidades/Concrect/ClienteBuilder.java
+++ entidades/Concrect/ClienteBuilder.java
@@ -2,34 +2,9 @@ package entidades.Concrect;
 
 import entidades.Abstract.Pessoa;
 
-public class ClienteBuilder {
-
-    private String nome;
-    private String cpf;
-    private String email;
-    private String telefone;
-
-    public ClienteBuilder nome(String nome) {
-        this.nome = nome;
-        return this;
+public class Cliente extends Pessoa {
+    public Cliente(String nome, String cpf, String email, String telefone) {
+        super(nome, cpf, email, telefone);
     }
 
-    public ClienteBuilder cpf(String cpf) {
-        this.cpf = cpf;
-        return this;
-    }
-
-    public ClienteBuilder email(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public ClienteBuilder telefone(String telefone) {
-        this.telefone = telefone;
-        return this;
-    }
-
-    public Pessoa build() {
-        return new Pessoa(nome, cpf, email, telefone);
-    }
 }
