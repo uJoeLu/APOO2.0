fbd9262ff979f9336c042e69f4ff79795c750861
Joel <jns18@discente.ifpe.edu.br>
2024-08-20 21:10:55 -0300

    organizacao

--- src/br/edu/ifpe/FormularioDeCadastro/Decorador/GratificacaoBase.java
+++ src/br/edu/ifpe/FormularioDeCadastro/Decorador/GratificacaoBase.java
@@ -1,16 +1,15 @@
 package br.edu.ifpe.FormularioDeCadastro.Decorador;
 
-
-
 public class GratificacaoBase implements IGratificacao {
     private double salarioBase;
-public GratificacaoBase(double salarioBase){
-    this.salarioBase = salarioBase;
-}
+
+    public GratificacaoBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
 
     @Override
     public double getSalario() {
         return salarioBase;
     }
-    
+
 }
